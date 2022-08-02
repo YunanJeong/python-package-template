c28d4e360cfc678849d4dbaf25f7be593d5200d6
yunanjeong <njhgle@gmail.com>
2022-08-02 18:56:57 +0900

    일부 수정

--- packagename/__main__.py
+++ packagename/__main__.py
@@ -5,15 +5,17 @@
 - 많은 패키지들은 직접실행할 수 있는 기능을 제공한다.
 - 직접실행시 __main__.py 가 프로그램의 시작점이 된다.
 
-- 여기서는 입력인자를 받아 처리하는 예시를 사용했다.
+- 여기서는 입력인자 3개(설정파일, 시간, 시간)를 받아 처리하는 예시를 사용했다.
+    - 실행시 시간인자를 입력하지 않으면 디폴트로 현재시간이 할당되는 예제이다.
 
 - 다음은 직접 실행방법의 예시이다. -m 옵션은 설치된 파이썬 패키지 경로를 참조할 떄 필요하다.
 - $ python -m -r"parameter1" -s"parameter2" -e"parameter3"
 
 """
-import os
 import argparse
 import util
+import datetime
+
 
 def main(required, start, end):
     util.example_func(required)
@@ -30,7 +32,7 @@ if __name__ == '__main__':
     parser.add_argument("-e", "--end", dest="end_param", action="store")
     args = parser.parse_args()
 
-    req = get_config(args.required_param)
+    req = util.example_read_config(args.required_param)
     sta = datetime.now() if args.startdate is None else datetime.strptime(args.start_param, '%Y%m%d') # NOQA
     end = datetime.now() if args.enddate is None else datetime.strptime(args.end_param, '%Y%m%d') # NOQA
 
--- packagename/util.py
+++ packagename/util.py
@@ -1,5 +1,12 @@
 def example_func(required):
     print(f'Required Parrameter: {required}')
+    return required
+
 
 def example_func_sub(sub_parameter):
     print(f'Required Parrameter: {sub_parameter}')
+
+
+def example_read_config(config_file):
+    print(f'configfile: {config_file}')
+    return 'example: parameter from configuration file'
